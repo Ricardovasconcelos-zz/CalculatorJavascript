e85003862327df8ff1cd9c7baabef5f3db98b092
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2019-02-19 16:38:07 -0300

    addOperators

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,81 +1,3 @@
-<<<<<<< HEAD
-  class CalcController {
-
-    constructor(){
-        this._displayCalc = document.querySelector('#display');
-        this._data = document.querySelector('#data');
-        this._time = document.querySelector('#hora');  
-        this._dataAtual;
-        this.initialize();
-        this.initButtonsEvents();
-    }
-
-    initialize(){ 
-        this.setDisplayDateTime();
-
-        setInterval(()=>{
-            this.setDisplayDateTime();       
-        },1000);
-    }
-    
-    initButtonsEvents(){
-        let $buttons = document.querySelectorAll('#buttons > g, #parts > g')
-        
-        $buttons.forEach((button, index)=>{
-            
-            this.addEventListenerAll(button, 'click drag', e =>{
-                console.log(button.className.baseVal.replace('btn-',''))
-                
-            })
-        })
-    }
-    
-    addEventListenerAll(elements, events, fn){
-        
-        events.split(' ').forEach(events => {
-            elements.addEventListener(events,fn,false)
-        });
-        
-
-    }
-
-    setDisplayDateTime(){
-        this.displayDate = this.dataAtual.toLocaleDateString('pt-br',{
-        day:"2-digit",
-        month:"long",
-        year:"numeric"})
-        this.displayTime = this.dataAtual.toLocaleTimeString('pt-br')
-    }
-
-    get displayCalc(){
-        return this._displayCalc.innerHTML;
-    }  
-    set displayCalc(value){
-        this._displayCalc.innerHTML = value;
-    }
-
-    get displayDate(){
-        return this._data.innerHTML;
-    }  
-    set displayDate(value){
-        this._data.innerHTML = value;
-    }
-
-    get dataAtual(){
-        return new Date();
-    }
-    set dataAtual(value){
-        this._dataAtual.innerHTML = value;
-    }
-
-    get displayTime(){
-        return this._time.innerHTML;
-    }
-    set displayTime(value){
-        this._time.innerHTML = value;
-    }
-}
-=======
   class CalcController {
 
     constructor(){
@@ -185,19 +107,14 @@
         }
     }
 
-    
-
     addOperation(value){
-
-        
-
+      
         if(isNaN(this.getLastOperation())){
 
             if(this.isOperator(value)){
 
                 this.setLastOperation(value);
-                console.log('ricardo',value)
-
+                
                 }else if(isNaN(value)){
                     console.log(value)
                 }else{
@@ -217,11 +134,9 @@
     isOperator(value){
         return (['+','-','*','%','/'].indexOf(value) > -1);
     }
-
     getLastOperation(){
         return this._operation[this._operation.length-1];
     }
-
     
     clearAll(){
         this._operation = [];
@@ -233,8 +148,6 @@
         this.displayCalc = 'Error'
     }
  
-
-
     //------------- GET AND SETS ----------------
     get displayCalc(){
         return this._displayCalc.innerHTML;
@@ -287,4 +200,3 @@
 
 
 
->>>>>>> development
