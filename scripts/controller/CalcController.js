5347412ae25c1488c7b1dc861dfd28f36a330646
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2019-02-20 19:40:59 -0300

    addCalc

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -118,16 +118,52 @@
                 }else if(isNaN(value)){
                     console.log(value)
                 }else{
-                    this._operation.push(value)
+                    this.pushOperation(value)
+                    this.setLastNumberToDisplay();
                 }
         }else{
-            let newValue = this.getLastOperation().toString() + value.toString()
-            this.setLastOperation(parseInt(newValue));
+             
+            if (this.isOperator(value)){
+                this.pushOperation(value)
+            }else{
+                let newValue = this.getLastOperation().toString() + value.toString()
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+            }
+
         }
         
         console.log(this._operation)
     }
+    calc(){
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(''))
 
+        this._operation = [result,last];
+        this.setLastNumberToDisplay();
+    }
+
+    pushOperation(value){
+        this._operation.push(value);
+        if(this._operation.length > 3){
+            
+            this.calc();
+        }
+
+    }
+    
+    setLastNumberToDisplay(){
+        let lastNumber;
+        for (let i = this._operation.length-1; i >= 0; i--){
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
+    }
     setLastOperation(value){
         this._operation[this._operation.length-1] = value;
     }
